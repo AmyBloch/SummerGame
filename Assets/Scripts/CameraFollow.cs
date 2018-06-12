ef935d9dacca6b3d8a157757fdbe84d74dffe274
theronhowe <theronhowe@wustl.edu>
2018-06-11 20:53:59 -0500

    add player movement script and mouse orbit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public Vector3 offset;
+    public float rotationSpeed;
+
+	void Start()
+    {
+        offset = this.transform.position - target.transform.position;
+	}
+	
+	void LateUpdate()
+    {
+        Quaternion rotationAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+        offset = rotationAngle * offset;
+        this.transform.position = target.transform.position + offset;
+        this.transform.LookAt(target.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    public float movespeed;
+
+    private Vector3 movement;
+
+    void Start()
+    {
+        movement = new Vector3();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * movespeed;
+        movement.z = Input.GetAxis("Vertical") * movespeed;
+        movement.y += Physics.gravity.y;
+        movement *= Time.deltaTime;
+        characterController.Move(movement);
+    }
+}
